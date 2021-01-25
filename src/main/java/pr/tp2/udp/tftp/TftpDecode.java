e3033b4863d38e9592e215ef5ff3fe8cf90b8da4
Valentin Hulot <valentin.hulot@etudiant.univ-rennes1.fr>
2021-01-25 22:55:55 +0100

    Part 1 complete

--- src/main/java/pr/tp2/udp/tftp/TftpDecode.java
+++ src/main/java/pr/tp2/udp/tftp/TftpDecode.java
@@ -1,23 +1,63 @@
 package pr.tp2.udp.tftp;
 
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Arrays;
 
 public class TftpDecode {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SocketException {
 		// Attends sur le port 6969
+		int serverPort = 6969;
+		DatagramSocket serverSocket =new DatagramSocket(serverPort);
 
-		// Boucle
+		try {
+			int packetNumber = 1;
+			byte buffer[] = new byte[100];
+			// Boucle
+			while(true){
 
-		// Reception du packet
-		DatagramPacket p = null;
+				// Reception du packet
+				DatagramPacket reception = new DatagramPacket(buffer, buffer.length);
+				serverSocket.receive(reception);
 
-		// Affichage du packet
+				// Affichage du packet
+				System.out.println("PACKET NUMBER "+packetNumber+" :");
+				//Whole buffer
+				System.out.println("Whole buffer :");
+				affiche(buffer);
+				System.out.println();
 
-		// Attention à ne pas afficher plus d'informations que nécessaire.
+				//Truncated buffer
+				System.out.println("Truncated buffer :");
+				int length = reception.getLength();
+				byte [] copy = Arrays.copyOfRange(buffer,0,length);
+				affiche(copy);
+				System.out.println();
 
-		// Décodage du packet
-		decodeRequest(p);
+				// Attention à ne pas afficher plus d'informations que nécessaire.
+				// ?
+				// Décodage du packet
+				System.out.println("Decoded packet :");
+
+				decodeRequest(reception);
+				System.out.println();
+
+				Arrays.fill(buffer, (byte) 0);
+				packetNumber++;
+				System.out.println("----------------------------------------");
+			}
+		}
+		catch(Exception e){
+			System.out.println("Error : "+e.getMessage());
+		}
+		finally {
+			serverSocket.close();
+			System.out.println("Server closed.");
+
+		}
+
 	}
 
 	public static void affiche(byte[] bytes) {
--- src/main/java/pr/tp2/udp/tftp/TftpPutServeur.java
+++ src/main/java/pr/tp2/udp/tftp/TftpPutServeur.java
@@ -1,39 +1,80 @@
 package pr.tp2.udp.tftp;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketAddress;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public class TftpPutServeur {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SocketException {
 		// Attends sur le port 6969
+		int serverPort = 6969;
+		DatagramSocket serverSocket =new DatagramSocket(serverPort);
 
-		// Boucle
+		try {
+			short packetNumber = 1;
+			byte buffer[] = new byte[100];
+			// Boucle
+			while(true){
 
-		// Reception du packet
-		DatagramPacket p = null;
+				// Reception du packet
+				DatagramPacket reception = new DatagramPacket(buffer, buffer.length);
+				serverSocket.receive(reception);
 
-		// Affichage du packet
+				//Truncated buffer
+				int length = reception.getLength();
+				byte [] copy = Arrays.copyOfRange(buffer,0,length);
+				affiche(copy);
+				System.out.println();
 
-		// Attention à ne pas afficher plus d'informations que nécessaire.
+				// Attention à ne pas afficher plus d'informations que nécessaire.
+				// ?
+				// Décodage du packet
+				System.out.println("Decoded packet :");
 
-		// Décodage du packet
-		decodeRequest(p);
+				decodeRequest(reception);
+				System.out.println();
 
-		// Envoyer acquittement
-		// sendAck(server, /***/);
+				Arrays.fill(buffer, (byte) 0);
+				packetNumber++;
+				System.out.println("----------------------------------------");
+
+				// Envoyer acquittement
+				sendAck(serverSocket, (short) packetNumber, reception);
+			}
+		}
+		catch(Exception e){
+			System.out.println("Error : "+e.getMessage());
+		}
+		finally {
+			serverSocket.close();
+			System.out.println("Server closed.");
+
+		}
+
 	}
-
-	public static void sendAck(DatagramSocket server, short seqNumber, SocketAddress dstAddr) throws IOException {
-		System.out.println("Send " + seqNumber + " to " + dstAddr);
+	//SocketAddress -> DatagramPacket
+	public static void sendAck(DatagramSocket server, short seqNumber, DatagramPacket dstAddr) throws IOException {
 
 		// Construire le paquet avec les bonnes informations
+		ByteBuffer byteBuffer = ByteBuffer.allocate(4);
+		byteBuffer.putShort((short) 4); //code for ack : 4, https://tools.ietf.org/html/rfc1350
+		byteBuffer.putShort(seqNumber);
 
 		// afficher le tableau de bytes envoyé
+		byte [] buffer = byteBuffer.array();
+		System.out.println("Buffer to send :");
+		affiche(buffer);
+		System.out.println();
 
 		// Envoyer le paquet à la bonnes addresses
+		DatagramPacket paquetEnvoie = new DatagramPacket(buffer, buffer.length, new InetSocketAddress(dstAddr.getAddress(), dstAddr.getPort()));
+		server.send(paquetEnvoie);
+		System.out.println("Send "+seqNumber+" to "+dstAddr.getAddress()+":"+dstAddr.getPort());
+		System.out.println("----------------------------------------");
+
+
 	}
 
 	public static void affiche(byte[] bytes) {
